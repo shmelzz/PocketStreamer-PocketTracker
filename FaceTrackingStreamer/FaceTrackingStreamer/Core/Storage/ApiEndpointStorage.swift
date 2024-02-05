f793da9e121f15589ec4cfd116509f7a3eda3892
Elizaveta Shelemekh <shelemekhliza0313@gmail.com>
2024-02-06 01:37:20 +0300

    meta-22: new EndpointStorage scheme

--- FaceTrackingStreamer/FaceTrackingStreamer/Core/Storage/ApiEndpointStorage.swift
+++ FaceTrackingStreamer/FaceTrackingStreamer/Core/Storage/ApiEndpointStorage.swift
@@ -14,3 +14,22 @@ protocol IApiEndpointStorage: AnyObject {
 }
 
 final class ApiEndpointStorage: UserDefaultsStorage<ApiEndpoint>, IApiEndpointStorage {}
+
+// MARK: - new
+
+struct EnvironmentStorage: UserDefaultsStorable {
+    static let key = "environmentKeys"
+    
+    let environments: [EnvironmentEndpoint]
+}
+
+struct EnvironmentEndpoint: Codable {
+    let environment: EnvironmentType
+    let endpoint: String
+    let port: String
+    
+    enum EnvironmentType: Codable {
+        case debug
+        case release
+    }
+}
