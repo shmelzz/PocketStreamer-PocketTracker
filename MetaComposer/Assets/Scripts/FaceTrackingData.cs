d02f6418171a4fd37ff0b0a1d5e80659a410d97f
teddyzxcv <48234309+teddyzxcv@users.noreply.github.com>
2024-02-23 13:10:58 +0300

    Add face mapping

---
+++ MetaComposer/Assets/Scripts/FaceTrackingData.cs
@@ -0,0 +1,59 @@
+namespace MetaComposer.Assets
+{
+    public class FaceTrackingData
+    { 
+        public double timeCode { get; set; }
+        public double MouthStretchRight { get; set; }
+        public double MouthRollUpper { get; set; }
+        public double BrowOuterUpRight	 { get; set; }
+        public double EyeLookUpLeft { get; set; }
+        public double EyeSquintRight { get; set; }
+        public double MouthLeft { get; set; }
+        public double BrowDownRight { get; set; }
+        public double MouthFrownRight { get; set; }
+        public double EyeBlinkRight { get; set; }
+        public double MouthFrownLeft { get; set; }
+        public double EyeLookDownRight { get; set; }
+        public double EyeSquintLeft { get; set; }
+        public double EyeLookOutLeft { get; set; }
+        public double JawForward { get; set; }
+        public double MouthRollLower { get; set; }
+        public double MouthDimpleLeft { get; set; }
+        public double MouthSmileLeft { get; set; }
+        public double EyeWideLeft { get; set; }
+        public double BrowOuterUpLeft { get; set; }
+        public double EyeLookUpRight { get; set; }
+        public double MouthShrugLower { get; set; }
+        public double BrowInnerUp { get; set; }
+        public double MouthUpperUpRight { get; set; }
+        public double MouthClose { get; set; }
+        public double JawLeft { get; set; }
+        public double MouthStretchLeft { get; set; }
+        public double JawRight { get; set; }
+        public double JawOpen { get; set; }
+        public double CheekSquintRight { get; set; }
+        public double EyeLookInLeft { get; set; }
+        public double MouthDimpleRight { get; set; }
+        public double MouthPucker { get; set; }
+        public double BrowDownLeft { get; set; }
+        public double MouthLowerDownLeft { get; set; }
+        public double MouthUpperUpLeft { get; set; }
+        public double NoseSneerRight { get; set; }
+        public double EyeLookDownLeft { get; set; }
+        public double MouthRight { get; set; }
+        public double MouthPressRight { get; set; }
+        public double MouthPressLeft { get; set; }
+        public double CheekPuff { get; set; }
+        public double CheekSquintLeft { get; set; }
+        public double EyeLookInRight { get; set; }
+        public double MouthSmileRight { get; set; }
+        public double MouthShrugUpper { get; set; }
+        public double EyeBlinkLeft { get; set; }
+        public double MouthFunnel { get; set; }
+        public double NoseSneerLeft { get; set; }
+        public double MouthLowerDownRight { get; set; }
+        public double EyeLookOutRight { get; set; }
+        public double EyeWideRight { get; set; }
+        public double TongueOut { get; set; }
+    } 
+}
---
+++ MetaComposer/Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRM;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        var proxy = GetComponent<VRMBlendShapeProxy>();
+        if (Input.GetKeyDown("space"))
+        {
+            proxy.SetValues(new Dictionary<BlendShapeKey, float>
+            {
+                {BlendShapeKey.CreateUnknown("JawOpen"), 1f},
+            });
+            Debug.Log("Space pressed");
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 2, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            proxy.SetValues(new Dictionary<BlendShapeKey, float>
+            {
+                {BlendShapeKey.CreateUnknown("JawOpen"), 0f},
+            });
+            Debug.Log("C pressed");
+        }
+    }
+}
